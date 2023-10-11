45904eafda7e75520cfd85ae29d3c7edeee0524d
Gaspar Acevedo <acevedo.zain.gaspar@gmail.com>
2023-10-11 17:35:26 -0300

    [kv-secrets-provider] remove unused variables

--- deployments/kv-secrets-provider/main.tf
+++ deployments/kv-secrets-provider/main.tf
@@ -1,10 +1,6 @@
 terraform {
   required_version = ">= 1.3.7"
   backend "azurerm" {
-    resource_group_name  = var.backend_rg_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = "terraform.tfstate"
   }
 }
 
--- deployments/kv-secrets-provider/variables.tf
+++ deployments/kv-secrets-provider/variables.tf
@@ -3,21 +3,3 @@ variable "environment" {
   type        = string
   default     = "test"
 }
-
-variable "backend_rg_name" {
-  description = "The azurerm backend resource group name."
-  type        = string
-  default     = "rg-gasper"
-}
-
-variable "backend_storage_account_name" {
-  description = "The azurerm backend storage account name."
-  type        = string
-  default     = "sagasperbackend"
-}
-
-variable "backend_container_name" {
-  description = "The azurerm backend container name."
-  type        = string
-  default     = "gaztfstate"
-}
